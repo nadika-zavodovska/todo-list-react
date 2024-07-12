c71eeba31e286f9a5a4e808ea82262f0de2c4fb6
Nadika <nadika.name@gmail.com>
2024-07-12 18:17:48 +0100

    feat: add delete functionality

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -16,7 +16,8 @@ function ToDoList(){
         }
     }
     function deleteTask(index){
-
+        const updatedTasks = tasks.filter((_, i) => i !== index);
+        setTasks(updatedTasks);
     }
     function moveTaskUp(index){
 

9485d4e61cc8b591aa24d995678ddd2c65d122fe
Nadika <nadika.name@gmail.com>
2024-07-12 17:12:18 +0100

    init: start todo list

---
+++ src/ToDoList.jsx
@@ -0,0 +1,12 @@
+import React, {useState} from "react";
+
+function ToDoList(){
+    const[tasks, setTasks] = useState([]);
+    const[newTask, setNewTask] = useState("");
+
+    return(
+        <></>
+    )
+}
+
+export default ToDoList;

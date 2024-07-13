34dc9c68ab5e265bedefb5acf0170c9c1dbdb4e0
Nadika <nadika.name@gmail.com>
2024-07-13 21:24:57 +0100

    feat: add move down function

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -20,11 +20,21 @@ function ToDoList(){
         setTasks(updatedTasks);
     }
     function moveTaskUp(index){
-
+        if(index > 0){
+            const updatedTasks = [...tasks];
+            [updatedTasks[index], updatedTasks[index - 1]] =
+            [updatedTasks[index - 1], updatedTasks[index]];
+            setTasks(updatedTasks);
+        }
     }
 
     function moveTaskDown(index){
-        
+        if(index < tasks.length - 1){
+            const updatedTasks = [...tasks];
+            [updatedTasks[index], updatedTasks[index + 1]] =
+            [updatedTasks[index + 1], updatedTasks[index]];
+            setTasks(updatedTasks);
+        }
     }
 
 
